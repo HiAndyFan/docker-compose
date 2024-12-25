bca047167722dd365cd671920016c486fd33d1c2
AndyF <Redmi-6800H>
2024-12-25 20:36:25 +0800

    edit template

--- nginx/certbot.sh
+++ nginx/certbot.sh
@@ -1,7 +1,6 @@
 sudo docker run -it --rm --name certbot \
     -v "./cloudflare.ini:/root/.secrets/cloudflare.ini"\
-    -v "./ssl:/etc/letsencrypt/live" \
-    -v "./certbot_conf/:/etc/letsencrypt/" \
+    -v "./ssl/:/etc/letsencrypt/" \
     -v "./log/:/var/log/letsencrypt/" \
     certbot/dns-cloudflare certonly \
         --preferred-challenges dns-01 \
--- x-install.sh
+++ x-install.sh
@@ -2,3 +2,5 @@
 sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
 
 sudo apt update
+
+sudo apt install fastfetch
